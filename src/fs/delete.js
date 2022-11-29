680c508f39ea6463f286e64999ed40064241b85f
Denis Abrosimov <darkpers@gmail.com>
2022-11-29 21:18:16 +0300

    fs/delete.js solution

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { rm } from 'fs/promises'
+import { join } from 'path'
+
 const remove = async () => {
-    // Write your code here 
+
+	let path = './files';
+	let fileName = 'fileToRemove.txt';
+	let errorMessage = 'FS operation failed';
+
+	try {
+		let fullFileName = join(path, fileName);
+		await rm(fullFileName);
+
+	} catch (err) {
+		console.error(errorMessage);
+	}
+
 };
 
 await remove();
