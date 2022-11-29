9c6907de5a414e51635205fa55c83c04e3781077
Denis Abrosimov <darkpers@gmail.com>
2022-11-29 15:52:46 +0300

    fs/create.js solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { writeFile } from 'fs/promises';
+import { join } from 'path';
+
 const create = async () => {
-    // Write your code here 
+	let path = './files';
+	let fileName = 'fresh.txt';
+	let fileContent = 'I am fresh and young';
+	let errorMessage = 'FS operation failed';
+
+	try {
+		let fullFileName = join(path, fileName);
+		let promise = writeFile(fullFileName, fileContent, {flag: 'wx'});
+		await promise;
+	} catch(err) {
+		console.log(errorMessage);
+	}
 };
 
 await create();
