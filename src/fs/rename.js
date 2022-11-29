04493185db0e7908c1942b2447d0537675a64860
Denis Abrosimov <darkpers@gmail.com>
2022-11-29 21:08:56 +0300

    fs/rename.js solution

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import { rename as rename2 } from 'fs/promises'
+import { join } from 'path'
+
 const rename = async () => {
-    // Write your code here 
+	let path = './files';
+	let oldFileName = 'wrongFilename.txt';
+	let newFileName = 'properFilename.md';
+	let errorMessage = 'FS operation failed';
+
+	try {
+		let oldPath = join(path, oldFileName);
+		let newPath = join(path, newFileName);
+
+		await rename2(oldPath, newPath);
+
+	} catch(err) {
+		console.error(errorMessage);
+	}
 };
 
 await rename();
