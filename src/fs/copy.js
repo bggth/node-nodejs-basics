ff87e1802adc477fc816564d948524ec3b6c6cd8
Denis Abrosimov <darkpers@gmail.com>
2022-11-29 20:22:29 +0300

    fs/copy.js solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import { readdir, copyFile, constants, mkdir } from 'fs/promises';
+import { join } from 'path';
+
 const copy = async () => {
-    // Write your code here 
+
+	let sourceFolder = './files';
+	let destinationFolder = './files_copy';
+	let errorMessage = 'FS operation failed';
+
+	try {
+		let files = await readdir(sourceFolder);
+
+		if (files.length > 0)
+			await mkdir(destinationFolder);
+
+		for (let file of files) {
+			let sourceFileName = join(sourceFolder, file);
+			let destinationFileName = join(destinationFolder, file);
+			await copyFile(sourceFileName, destinationFileName, constants.COPYFILE_EXCL);
+		}
+
+	} catch(err) {
+		console.error(errorMessage);
+	}
+
+
 };
 
 copy();
